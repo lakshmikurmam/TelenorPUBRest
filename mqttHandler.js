1f8c9a66a5c3e61c875a316bb7f35c6720c20a4e
Lakshmi <Rajyalakshmi.gudimetla@ge.com>
2023-05-15 20:33:11 +0530

    changes pushed v2

--- mqttHandler.js
+++ mqttHandler.js
@@ -1,10 +1,10 @@
 const mqtt = require('mqtt');
 
 class MQTTHandler {
-    constructor(brokerUrl, clientId, secretKey) {
+    constructor(brokerUrl, username, password) {
         brokerUrl = '10.246.0.10:1883'
         this.client = mqtt.connect(brokerUrl, {
-            clientId: 'gepc-producer',
+            //this.clientId = 'gepc-producer',
             username: '95cb7981-3d4c-4b35-aff0-73d5150be1be',
             password: 'Bearer 95cb7981-3d4c-4b35-aff0-73d5150be1be:gepc-producer:26afc6e1'
 
@@ -17,6 +17,19 @@ class MQTTHandler {
         });
     }
 
+
+    connect() {
+        console.log("Step111111")
+        const mqttClient = mqtt.connect('mqtt://10.246.0.10:1883', '95cb7981-3d4c-4b35-aff0-73d5150be1be', 'Bearer 95cb7981-3d4c-4b35-aff0-73d5150be1be:gepc-producer:26afc6e1');
+        console.log("Step112")
+        mqttClient.on('connect', () => {
+            console.log('Connected to MQTT broker');
+        });
+
+        mqttClient.on('error', (err) => {
+            console.log(`MQTT error: ${err}`);
+        });
+    }
     subscribe(topic) {
         this.client.subscribe(topic, (err) => {
             if (err) {
--- publish.js
+++ publish.js
@@ -1,4 +1,4 @@
-var express = require("express");
+/*var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var mqttHandler = require('./mqttHandler');
@@ -7,9 +7,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 var mqttClient = new mqttHandler();
-//mqttClient.connect();
-
-// Routes
+console.log("Step1")
+mqttClient.connect();
+console.log("Step2")
+    // Routes
 app.post("/send-mqtt", function(req, res) {
     mqttClient.publish(req.body.message);
     res.status(200).send("Message sent to mqtt");
@@ -17,4 +18,43 @@ app.post("/send-mqtt", function(req, res) {
 
 var server = app.listen(8000, function() {
     console.log("app running on Rest api port.", server.address().port);
+});
+
+*/
+const express = require('express');
+const mqtt = require('mqtt');
+
+const app = express();
+const port = 3000;
+
+var bodyParser = require("body-parser");
+
+console.log('Trying to connect');
+const mqttClient = mqtt.connect('mqtt://10.246.0.10:1883', '95cb7981-3d4c-4b35-aff0-73d5150be1be', 'Bearer 95cb7981-3d4c-4b35-aff0-73d5150be1be:gepc-producer:26afc6e1');
+
+mqttClient.on('connect', () => {
+    console.log('Connected to MQTT broker');
+});
+
+mqttClient.on('error', (err) => {
+    console.log(`MQTT error: ${err}`);
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.post('/publish', (req, res) => {
+    // const { topic, message } = req.body;
+
+    mqttClient.publish(req.body.message, (err) => {
+        if (err) {
+            console.log(`Failed to publish message: ${err}`);
+            res.status(500).send('Failed to publish message');
+        } else {
+            console.log(`Message published to topic`);
+            res.status(200).send(`Message published to topic`);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`REST API listening at http://localhost:${port}`);
 });
