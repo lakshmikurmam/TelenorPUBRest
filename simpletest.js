7f1cc581b7793decbe9cbb132428d7cd795c3c22
Lakshmi <Rajyalakshmi.gudimetla@ge.com>
2023-05-16 16:20:55 +0530

    Added Docker Files

--- simpletest.js
+++ simpletest.js
@@ -1,15 +1,20 @@
 const mqtt = require('mqtt');
-const client = mqtt.connect('mqtt://10.246.0.10:1883', 'Bearer 95cb7981-3d4c-4b35-aff0-73d5150be1be:gepc-producer:26afc6e1');
+
+const username = 'gepc-producer';
+const password = '26afc6e1';
+const clientId = '95cb7981-3d4c-4b35-aff0-73d5150be1be';
+const host = '10.246.0.10:1883';
+
+const client = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
+    clientId,
+    clean: true,
+    rejectUnauthorized: false
+});
 
 client.on('connect', () => {
-    console.log('Connected to broker');
+    console.log('Connected to Telenor Connexion MQTT broker');
+});
 
-    // Publish a message to the topic 'test'
-    client.publish('test', 'Hello, world!', { qos: 1 }, (err, packet) => {
-        if (err) {
-            console.error('Error publishing message:', err);
-        } else {
-            console.log('Message published:', packet);
-        }
-    });
+client.on('error', (err) => {
+    console.error('Error connecting to Telenor Connexion MQTT broker:', err);
 });
