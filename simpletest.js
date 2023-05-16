f30df17af654bd5c2ee3711c36f828f9e5a48364
Lakshmi <Rajyalakshmi.gudimetla@ge.com>
2023-05-16 14:40:20 +0530

    Added only secret key

--- simpletest.js
+++ simpletest.js
@@ -1,5 +1,5 @@
 const mqtt = require('mqtt');
-const client = mqtt.connect('mqtt://10.246.0.10:1883', '95cb7981-3d4c-4b35-aff0-73d5150be1be', 'gepc-producer', 'Bearer 95cb7981-3d4c-4b35-aff0-73d5150be1be:gepc-producer:26afc6e1');
+const client = mqtt.connect('mqtt://10.246.0.10:1883', 'Bearer 95cb7981-3d4c-4b35-aff0-73d5150be1be:gepc-producer:26afc6e1');
 
 client.on('connect', () => {
     console.log('Connected to broker');
