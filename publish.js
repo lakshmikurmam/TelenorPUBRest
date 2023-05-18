472bb2efae01dc9dcf4395d39236f644c2c755df
Lakshmi <Rajyalakshmi.gudimetla@ge.com>
2023-05-18 15:05:10 +0530

    Error fixed

--- publish.js
+++ publish.js
@@ -6,14 +6,15 @@ const clientId = '95cb7981-3d4c-4b35-aff0-73d5150be1be';
 const host = '10.246.0.10:1883';
 const MQTT_TOPIC = 'MC/V1/testing';
 const MQTT_QOS = 1;
+let MQTT_MESSAGE;
 
-const client = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
+const client1 = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
     clientId,
     clean: true,
     rejectUnauthorized: false
 });
-
-client.on('connect', () => {
+var client = mqtt.connect('mqtt://test.mosquitto.org:1883');
+client.on('connect', function(topic, message, packet) {
     console.log('Connected to Telenor Connexion MQTT broker');
 });
 // Publish message to MQTT topic
@@ -26,13 +27,20 @@ setInterval(function() {
     let rand = Math.random() * 100;
     let sec = (Math.floor(ts / 1000));
     const id = crypto.randomBytes(16).toString("hex");
-    MQTT_MESSAGE = 'year + "-" + month + "-" + date + ":" + sec, "log generated MQ messages publishing :" + rand + ":", "seqid:" + id '
-    client.publish(MQTT_TOPIC, MQTT_MESSAGE, { qos: MQTT_QOS }, String(rand), function(err) {
+    const s = "log generated MQ messages publishing "
+    const seq = "seqid:"
+        //console.log(year + "-" + month + "-" + date + ":" + sec, "log generated MQ messages publishing :" + rand + ":", "seqid:" + id)
+    MQTT_MESSAGE = year + "-" + month + "-" + date + ":" + sec, +s + +rand + ":", +seq + +id
+        //'year + "-" + month + "-" + date + ":" + sec, "log generated MQ messages publishing :" + rand + ":", "seqid:" + id '
+    client.publish(MQTT_TOPIC, MQTT_MESSAGE, { qos: MQTT_QOS }, function(err) {
+
+        // client.publish(MQTT_TOPIC, MQTT_MESSAGE, { qos: MQTT_QOS }, String(rand), function(err) {
         if (err) {
             console.error(`Failed to publish message: ${err}`);
         } else {
-            console.log(`Message published to topic: ${MQTT_TOPIC}`);
-            // Disconnect from MQTT broker
+
+            console.log(`Message published to topic: ${MQTT_TOPIC} : ${MQTT_MESSAGE} ${s} ${seq}${id}`);
+
             client.end();
             console.log(`Connection END`);
         }
