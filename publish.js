a555026e3ce864ab91c23ccf05a569f54c400c0d
Lakshmi <Rajyalakshmi.gudimetla@ge.com>
2023-05-18 18:48:53 +0530

    Changes pushedv2.4

--- publish.js
+++ publish.js
@@ -8,8 +8,8 @@ let messageCount = 0;
 const MQTT_TOPIC = 'MC/V1/testing';
 const MQTT_QOS = 1;
 //Testing
-//var client = mqtt.connect('mqtt://test.mosquitto.org:1883');
-const client = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
+var client = mqtt.connect('mqtt://test.mosquitto.org:1883');
+const client1 = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
     clientId,
     clean: true,
     rejectUnauthorized: false
@@ -22,29 +22,29 @@ client.on('connect', () => {
 
 
     setInterval(function publishmessage() {
-        if (messageCount < 5) {
-            let ts = Date.now();
+        //if (messageCount < 5) {
+        let ts = Date.now();
 
-            let date_ob = new Date(ts);
-            let date = date_ob.getDate();
-            let month = date_ob.getMonth() + 1;
-            let year = date_ob.getFullYear();
-            let rand = Math.random() * 100;
-            let sec = (Math.floor(ts / 1000));
-            const id = crypto.randomBytes(16).toString("hex");
-            let s1 = 'log generated MQ messages publishing :'
-            const messageprint = year + "-" + month + "-" + date + ":" + sec + s1 + rand + ":"
-            "seqid:" + id
-            const message = 'Published message ' + messageprint + (messageCount + 1);
-            client.publish(MQTT_TOPIC, message, { qos: MQTT_QOS });
-            messageCount++;
+        let date_ob = new Date(ts);
+        let date = date_ob.getDate();
+        let month = date_ob.getMonth() + 1;
+        let year = date_ob.getFullYear();
+        let rand = Math.random() * 100;
+        let sec = (Math.floor(ts / 1000));
+        const id = crypto.randomBytes(16).toString("hex");
+        let s1 = 'log generated MQ messages publishing :'
+        const messageprint = year + "-" + month + "-" + date + ":" + sec + s1 + rand + ":"
+        "seqid:" + id
+        const message = 'Published message ' + messageprint;
+        client.publish(MQTT_TOPIC, message, { qos: MQTT_QOS });
+        messageCount++;
 
-            console.log('Published message:', message);
+        console.log('Published message:', message);
 
-            // setTimeout(publishmessage, 5000); // Publish a new message every 1 second
-            // } else {
-            //    endConnection();
-        }
+        // setTimeout(publishmessage, 5000); // Publish a new message every 1 second
+        // } else {
+        //    endConnection();
+        //}
 
     }, 4000)
 
@@ -54,9 +54,9 @@ client.on('connect', () => {
 client.on('error', (error) => {
     console.error('MQTT Error:', error);
 });
-client.on('close', () => {
-    console.log('Disconnected from MQTT broker');
-});
+//client.on('close', () => {
+//console.log('Disconnected from MQTT broker');
+//});
 
 function endConnection() {
     console.log('Ending MQTT connection');
